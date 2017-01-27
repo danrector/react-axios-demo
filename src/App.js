0e945a263b0c59cb133f2cfb4ae202758f794bd3
Daniel Rector <rector.danielk@gmail.com>
2017-01-27 17:53:31 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+var axios = require('axios');
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        jobs: []
+    };
+  }
+  
+  componentDidMount() {
+    // Is there a React-y way to avoid rebinding `this`? fat arrow?
+    var th = this;
+    this.serverRequest = 
+      axios.get(this.props.source)
+        .then(function(result) {    
+          th.setState({
+            jobs: result.data.jobs
+          });
+        })
+  }
+  
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Jobs!</h1>
+        {/* Don't have an ID to use for the key, URL work ok? */}
+        {this.state.jobs.map(function(job) {
+          return (
+            <div key={job.url} className="job">
+              <a href={job.url}>
+                {job.company_name}
+                is looking for a 
+                {job.term}
+                {job.title}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
